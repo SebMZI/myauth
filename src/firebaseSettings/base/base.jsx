0adf67b23a82bda214ac55af678a55e4bedee6f5
Sebastien Morazzani <mrgy.sebastien@gmail.com>
2023-05-22 17:21:19 +0200

    refactor: moins de warning

--- src/firebaseSettings/base/base.jsx
+++ src/firebaseSettings/base/base.jsx
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,7 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
-const analytics = getAnalytics(app);
 
 export default app;
 export { storage };
